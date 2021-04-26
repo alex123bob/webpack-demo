2f7a3a5f542899ce550470abe0728914c518ab89
alex123bob <alex123bob@126.com>
2021-04-26 10:33:45 +0800

    add file loader to load image

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+      rules: [
+          {
+              test: /\.(png|jpe?g|gif)/i,
+              use: [
+                  {
+                      loader: 'file-loader',
+                      options: {
+                          name: 'resources/[contenthash].[ext]'
+                      }
+                  }
+              ]
+          }
+      ]
+  }
 };
